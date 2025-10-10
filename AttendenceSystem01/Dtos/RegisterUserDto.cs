6dde6ae42f8e019ffd1f7e7d5e967687eb09179f
parasvanve <parasvanve218@gmail.com>
2025-10-10 17:48:47 +0530

    fix invalid email,name ,pasword

--- AttendenceSystem01/Dtos/RegisterUserDto.cs
+++ AttendenceSystem01/Dtos/RegisterUserDto.cs
@@ -1,12 +1,25 @@
-﻿namespace AttendenceSystem01.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendenceSystem01.Dtos
 {
         public class RegisterUserDto
         {
+            [Required]
+            [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
             public string FullName { get; set; }
+
+            [Required(ErrorMessage = "Email is required.")]
+
+            [RegularExpression(@"^[A-Za-z][A-Za-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$",
+            ErrorMessage = "Invalid email address format (cannot start with number).")]
+            [EmailAddress(ErrorMessage = "Invalid email address format.")]
             public string Email { get; set; }
+            [Required]
             public string Password { get; set; }
             public bool IsActive { get; set; } = true;
+            [Required]
             public int CreatedById { get; set; }
+             [Required]
             public List<int> RoleIds { get; set; } = new List<int>(); 
         }
 
--- AttendenceSystem01/Models/User.cs
+++ AttendenceSystem01/Models/User.cs
@@ -13,24 +13,31 @@ public partial class User
     public int UserId { get; set; }
 
     [StringLength(100)]
+    [Required]
     public string FullName { get; set; } = null!;
 
     [StringLength(100)]
+    [Required(ErrorMessage = "Email is required.")]
+    [RegularExpression(@"^[A-Za-z][A-Za-z0-9._%+-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$",
+        ErrorMessage = "Invalid email address format (cannot start with number).")]
+    [EmailAddress(ErrorMessage = "Invalid email address format.")]
     public string Email { get; set; } = null!;
 
     [StringLength(255)]
+    [Required]
     public string PasswordHash { get; set; } = null!;
 
     public bool IsActive { get; set; }
 
     [Column(TypeName = "datetime")]
     public DateTime CreatedAt { get; set; }
-
+    [Required]
     public int? CreatedById { get; set; }
 
     [InverseProperty("User")]
     public virtual ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
 
     [InverseProperty("User")]
+    [Required]
     public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
 }
